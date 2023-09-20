30091bb88560d68ba51688e248bc0fb6fe041e14
SahulKola <Mail2sahul.js@gmail.com>
2023-09-20 12:25:32 +0530

    Added search field validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -8,8 +8,13 @@ import { CardComponent } from './card/card.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, CardComponent, CardContainerComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    CardComponent,
+    CardContainerComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-
+  searchForm = new FormGroup({
+    text: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern(/^[A-Za-z0-9 ]*$/),
+    ]),
+  });
+  onSubmit() {
+    console.log(this.searchForm.value.text);
+    this.searchForm.controls.text.reset('');
+  }
 }
