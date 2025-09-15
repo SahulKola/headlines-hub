f38219ad4cb1ba5e5379e04bb9eced712ade1adf
Sai Kumar Kola <39924419+SahulKola@users.noreply.github.com>
2025-09-15 13:57:27 +0530

    update variableName from apiKey to api

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -22,18 +22,18 @@ export class NewsService {
   // apiKey = '&apikey=3a28524c4bdcd6927a65f24a01da97ba';
   // apiKey = '&apikey=e1d511adf953922221c06ffeb20180da';
   // apiKey = '&apikey=7383be19432b6f431a6cc47dcb26bc36';
-  apiKey = '&apikey=4674321b74af7cbe7066b26b0aca6de9';
+  api = '&apikey=4674321b74af7cbe7066b26b0aca6de9';
 
   baseEverythingUrl = 'https://gnews.io/api/v4/search?lang=en&q=';
 
   baseInUrl = `https://gnews.io/api/v4/top-headlines?lang=en&country=in&category=`;
   fetchIntNews(q: string): Observable<IRoot> {
-    const url = `${this.baseEverythingUrl}${q}${this.apiKey}`;
+    const url = `${this.baseEverythingUrl}${q}${this.api}`;
     return this.http.get<IRoot>(url);
   }
 
   fetchNews(category: string = ''): Observable<IRoot> | Observable<any> {
-    const url = `${this.baseInUrl}${category}${this.apiKey}`;
+    const url = `${this.baseInUrl}${category}${this.api}`;
     if (this.categories.includes(category) || category === '') {
       return this.http.get<IRoot>(url);
     } else {
